a038bba6f4841e622ff88b67492497e5df538467
uwu-alexandra <alexandra.sofronea27@gmail.com>
2025-08-08 22:55:44 +0200

    playwright setup

--- JavaScript Fundamentals/lessons/lesson9.js
+++
@@ -1,7 +0,0 @@
-import { CustomerDetails } from "../helpers/classCustomer.js";
-
-var customerDetails = new CustomerDetails();
-customerDetails.printFirstName("John Doe");
-
-import { car } from "../helpers/classCustomer.js";
-car.printCarName("Toyota");
