e268a941de7d63a51d84a478714d2cec721fb8e0
uwu-alexandra <alexandra.sofronea27@gmail.com>
2025-08-09 17:44:19 +0200

    using hooks

--- Module 2/pw-practice-app/tests/firstTest.spec.ts
+++ Module 2/pw-practice-app/tests/firstTest.spec.ts
@@ -1,5 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("the first test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
+});
+
+test.describe.only('suite1', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+
+  test("navigation to form layouts", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("navigation to datepicker", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
