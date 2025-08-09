845247a93317d13aba3ab44785ea4a80e8289c6d
uwu-alexandra <alexandra.sofronea27@gmail.com>
2025-08-09 17:12:41 +0200

    update setup folders

--- JavaScript Fundamentals/Module 2/tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("example test", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  await expect(page).toHaveTitle(/Playwright/);
-});
